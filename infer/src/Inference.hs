f2aaafb1d0f8114f2481fcb3f0b9f68e6e572b3b
Arjaz <arjaz@protonmail.com>
2023-02-03 10:38:08 +0200

    infer: use Control.Monad.Except.throwError instead of Left

--- infer/src/Inference.hs
+++ infer/src/Inference.hs
@@ -1,13 +1,13 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 -- That whole module is just an attempt at imlementing a simple bidirectional type checking
 -- It would have to be updated to reuse all the ast/type information from the rest of the project
 -- Also, currently there's no subsumption/∀ mechanism, but it would be needed for traits
 module Inference where
 
-import Control.Monad (unless)
+import Control.Monad.Except
 
 newtype Variable = Variable String
     deriving (Show, Eq, Ord)
@@ -44,7 +44,7 @@ synthesizeType c e = case e of
     EUnit -> pure TUnit
     EVariable v -> case lookup v c of
         Just t -> pure t
-        Nothing -> Left (TypeError "Variable type is unknown")
+        Nothing -> throwError (TypeError "Variable type is unknown")
     EAnnotation e2 t -> do
         checkType c e2 t
         pure t
@@ -52,22 +52,22 @@ synthesizeType c e = case e of
         t <- synthesizeType c e2
         (t1, t2) <- case t of
             t1 :-> t2 -> pure (t1, t2)
-            _ -> Left (TypeError "not a function")
+            _ -> throwError (TypeError "not a function")
         checkType c e3 t1
         pure t2
-    EFunction _ _ -> Left (RetardError "function expressions must be checked")
+    EFunction _ _ -> throwError (RetardError "function expressions must be checked")
 
 checkType :: Context -> Expression -> Type -> Inference ()
 checkType c e t = case e of
     EFunction v e2 -> case t of
         t1 :-> t2 -> checkType ((v, t1) : c) e2 t2
-        _ -> Left (TypeError "a function found")
-    EUnit -> unless (t == TUnit) (Left (TypeError "a unit found"))
+        _ -> throwError (TypeError "a function found")
+    EUnit -> unless (t == TUnit) (throwError (TypeError "a unit found"))
     EVariable v -> case lookup v c of
-        Just t1 -> unless (t == t1) (Left (TypeError "type error"))
-        Nothing -> Left (TypeError "unbound variable")
-    EAnnotation _ _ -> Left (RetardError "annotation types must be synthesized")
-    EApplication _ _ -> Left (RetardError "application types must be synthesized")
+        Just t1 -> unless (t == t1) (throwError (TypeError "type error"))
+        Nothing -> throwError (TypeError "unbound variable")
+    EAnnotation _ _ -> throwError (RetardError "annotation types must be synthesized")
+    EApplication _ _ -> throwError (RetardError "application types must be synthesized")
 
 λ = EFunction
 rv = EVariable . Variable
